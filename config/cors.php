23ecefca2a69f924717584ac271ed26c04cde1f3
Dhira <weirdodoughnut@gmail.com>
2025-07-12 23:49:21 +0700

    Fix CORS: allow Vercel frontend domain

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,15 @@
 <?php
+
 return [
-'paths' => ['api/*'],
-'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:5173'],
-'allowed_origins_patterns' => [],
-'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'max_age' => 0,
-'supports_credentials' => true,
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173', 
+        'https://web-bps-frontend-gules.vercel.app', 
+    ],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => true,
 ];
